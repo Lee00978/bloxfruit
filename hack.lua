e16c546bca5059b62b371966aea9bc9362095fb6
Lee00978 <132458192+Lee00978@users.noreply.github.com>
2023-05-03 21:03:25 +0800

    Update hack.lua

--- hack.lua
+++ hack.lua
@@ -1,12 +1,30 @@
-if game.PlacedID == 2753915549 then
-    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-    local Window = OrionLib:MakeWindow({Name = "Blox Fruits", HidePremium = false, SaveConfig = true, ConfigFolder = "BloxfruitHack"})
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Blox Fruit Hack v1.0", "DarkTheme")
+
+
+
+local Tab = Window:NewTab("Credits")
+local Section = Tab:NewSection("Developed by: LwLw")
+
+
+
+local Tab = Window:NewTab("Main")
+local Section = Tab:NewSection("Section Name")
+Section:NewButton("ButtonText", "ButtonInfo", function()
+    print("Clicked")
+end)
 
 
 
 
 
 
+local Tab = Window:NewTab("Setting")
+local Section = Tab:NewSection("Section Name")
 
-end
-    OrionLib:Init()
+
+
+
+
+local Tab = Window:NewTab("Weapons")
+local Section = Tab:NewSection("Section Name")
