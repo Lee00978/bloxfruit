3d181c24e87d1f97de83fe66a71db51290e22ebb
Lee00978 <132458192+Lee00978@users.noreply.github.com>
2023-05-03 20:01:36 +0800

    Create hack.lua

---
+++ hack.lua
@@ -0,0 +1,12 @@
+if game.PlacedID == 2753915549 then
+    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+    local Window = OrionLib:MakeWindow({Name = "Blox Fruits", HidePremium = false, SaveConfig = true, ConfigFolder = "BloxfruitHack"})
+
+
+
+
+
+
+
+end
+    OrionLib:Init()
